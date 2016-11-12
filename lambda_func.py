8137b07675a6278a6d8754e0d166b5924b500d6c
Arvind Ram <arvindram03@gmail.com>
2016-11-12 03:18:14 -0500

    adding trend for expenses

--- lambda_func.py
+++ lambda_func.py
@@ -9,14 +9,14 @@ def get_net_income():
 	txns = get_txns()
 	income = sum([txn['amount'] for txn in txns if txn['amount'] > 0])
 
-	s = "Your net income is {}".format(income)
+	s = "Your net income is ${}".format(income)
 	return s
 
 def get_net_expenditure():
 	txns = get_txns()
 	expenditure = -sum([txn['amount'] for txn in txns if txn['amount'] < 0])
 
-	s = "Your net expenditure is {}".format(expenditure)
+	s = "Your net expenditure is ${}".format(expenditure)
 	return s
 
 
@@ -55,14 +55,35 @@ def stat(flag):
 	resp = '{} {}.'.format(get_type(flag), items[:len(items)-1])
 	return resp
 
+def trend():
+	txns = get_txns()
+	cat = {}
+	for txn in txns:
+		if txn['amount'] < 0:
+			txn_cat = txn['category']
+			month = txn['date'][:6]
+			cat[txn_cat] = cat.get(txn_cat, {})
+			cat[txn_cat][month] = cat[txn_cat].get(month,0) + txn['amount']
+	
+	increase_trend = []
+
+	for c in cat:
+		cat[c] = sorted(cat[c].items(), key=lambda x:x[0])[-2:]
+
+		if len(cat[c]) > 1 and ((cat[c][1][1] - cat[c][0][1]) / cat[c][0][1] ) > .5:
+			increase_trend.append(c)
+	
+	resp = 'You drastically increased spending on {}.'.format(', '.join(increase_trend))
+	return resp
+
 	
 
 
 
 
-a = {}
-a['a'] = net_income
+# a = {}
+# a['a'] = net_income
 
 #print net_income()
 #print net_expenditure()
-print stat(-1)
+print trend()
